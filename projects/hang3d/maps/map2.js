f694dbb5a8a9391c32e657183f93453be1adba2d
zlatnaspirala <zlatnaspirala@gmail.com>
2024-11-17 22:35:05 +0100

    wip

--- projects/hang3d/maps/map2.js
+++ projects/hang3d/maps/map2.js
@@ -1,1 +1,82 @@
-export const map = {"staticCubes": [{"name": "wall_gen25_25", "position": {"x": "9", "y": 1, "z": "9"}, "scale": [1, 1, 1], "texture": {"source": ["res/images/diffuse.png"], "mix_operation": "multiply"}}, {"name": "wall_gen25_25", "position": {"x": "10", "y": 1, "z": "9"}, "scale": [1, 1, 1], "texture": {"source": ["res/images/diffuse.png"], "mix_operation": "multiply"}}, {"name": "wall_gen25_25", "position": {"x": "11", "y": 1, "z": "9"}, "scale": [1, 1, 1], "texture": {"source": ["res/images/diffuse.png"], "mix_operation": "multiply"}}, {"name": "wall_gen25_25", "position": {"x": "12", "y": 1, "z": "9"}, "scale": [1, 1, 1], "texture": {"source": ["res/images/diffuse.png"], "mix_operation": "multiply"}}, {"name": "wall_gen25_25", "position": {"x": "13", "y": 1, "z": "9"}, "scale": [1, 1, 1], "texture": {"source": ["res/images/diffuse.png"], "mix_operation": "multiply"}}, {"name": "wall_gen25_25", "position": {"x": "14", "y": 1, "z": "9"}, "scale": [1, 1, 1], "texture": {"source": ["res/images/diffuse.png"], "mix_operation": "multiply"}}, {"name": "wall_gen25_25", "position": {"x": "14", "y": 1, "z": "10"}, "scale": [1, 1, 1], "texture": {"source": ["res/images/diffuse.png"], "mix_operation": "multiply"}}, {"name": "wall_gen25_25", "position": {"x": "14", "y": 1, "z": "11"}, "scale": [1, 1, 1], "texture": {"source": ["res/images/diffuse.png"], "mix_operation": "multiply"}}, {"name": "wall_gen25_25", "position": {"x": "13", "y": 1, "z": "11"}, "scale": [1, 1, 1], "texture": {"source": ["res/images/diffuse.png"], "mix_operation": "multiply"}}, {"name": "wall_gen25_25", "position": {"x": "12", "y": 1, "z": "11"}, "scale": [1, 1, 1], "texture": {"source": ["res/images/diffuse.png"], "mix_operation": "multiply"}}, {"name": "wall_gen25_25", "position": {"x": "10", "y": 1, "z": "11"}, "scale": [1, 1, 1], "texture": {"source": ["res/images/diffuse.png"], "mix_operation": "multiply"}}, {"name": "wall_gen25_25", "position": {"x": "9", "y": 1, "z": "11"}, "scale": [1, 1, 1], "texture": {"source": ["res/images/diffuse.png"], "mix_operation": "multiply"}}, {"name": "wall_gen25_25", "position": {"x": "9", "y": 1, "z": "10"}, "scale": [1, 1, 1], "texture": {"source": ["res/images/diffuse.png"], "mix_operation": "multiply"}}]}
+export const map = {
+  staticCubes: [
+    {
+      name: "wall_gen25_25",
+      position: {x: "9", y: 1, z: "9"},
+      scale: [1, 1, 1],
+      texture: {source: ["res/images/diffuse.png"], mix_operation: "multiply"},
+    },
+    {
+      name: "wall_gen25_25",
+      position: {x: "10", y: 1, z: "9"},
+      scale: [1, 1, 1],
+      texture: {source: ["res/images/diffuse.png"], mix_operation: "multiply"},
+    },
+    {
+      name: "wall_gen25_25",
+      position: {x: "11", y: 1, z: "9"},
+      scale: [1, 1, 1],
+      texture: {source: ["res/images/diffuse.png"], mix_operation: "multiply"},
+    },
+    {
+      name: "wall_gen25_25",
+      position: {x: "12", y: 1, z: "9"},
+      scale: [1, 1, 1],
+      texture: {source: ["res/images/diffuse.png"], mix_operation: "multiply"},
+    },
+    {
+      name: "wall_gen25_25",
+      position: {x: "13", y: 1, z: "9"},
+      scale: [1, 1, 1],
+      texture: {source: ["res/images/diffuse.png"], mix_operation: "multiply"},
+    },
+    {
+      name: "wall_gen25_25",
+      position: {x: "14", y: 1, z: "9"},
+      scale: [1, 1, 1],
+      texture: {source: ["res/images/diffuse.png"], mix_operation: "multiply"},
+    },
+    {
+      name: "wall_gen25_25",
+      position: {x: "14", y: 1, z: "10"},
+      scale: [1, 1, 1],
+      texture: {source: ["res/images/diffuse.png"], mix_operation: "multiply"},
+    },
+    {
+      name: "wall_gen25_25",
+      position: {x: "14", y: 1, z: "11"},
+      scale: [1, 1, 1],
+      texture: {source: ["res/images/diffuse.png"], mix_operation: "multiply"},
+    },
+    {
+      name: "wall_gen25_25",
+      position: {x: "13", y: 1, z: "11"},
+      scale: [1, 1, 1],
+      texture: {source: ["res/images/diffuse.png"], mix_operation: "multiply"},
+    },
+    {
+      name: "wall_gen25_25",
+      position: {x: "12", y: 1, z: "11"},
+      scale: [1, 1, 1],
+      texture: {source: ["res/images/diffuse.png"], mix_operation: "multiply"},
+    },
+    {
+      name: "wall_gen25_25",
+      position: {x: "10", y: 1, z: "11"},
+      scale: [1, 1, 1],
+      texture: {source: ["res/images/diffuse.png"], mix_operation: "multiply"},
+    },
+    {
+      name: "wall_gen25_25",
+      position: {x: "9", y: 1, z: "11"},
+      scale: [1, 1, 1],
+      texture: {source: ["res/images/diffuse.png"], mix_operation: "multiply"},
+    },
+    {
+      name: "wall_gen25_25",
+      position: {x: "9", y: 1, z: "10"},
+      scale: [1, 1, 1],
+      texture: {source: ["res/images/diffuse.png"], mix_operation: "multiply"},
+    },
+  ],
+};
