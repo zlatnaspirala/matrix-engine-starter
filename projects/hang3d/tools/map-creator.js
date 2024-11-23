05eff76f3d0f7053840ec72b74c3cb7d808b477a
zlatnaspirala <zlatnaspirala@gmail.com>
2024-11-23 21:44:15 +0100

    MapCreator tool

--- projects/hang3d/tools/map-creator.js
+++ projects/hang3d/tools/map-creator.js
@@ -1,13 +1,21 @@
-var byId= (id) => {
+var byId = (id) => {
 	return document.getElementById(id)
 }
 
 var mapCreator = {
+	isMousePressed: false,
+	clear: () => {
+		location.reload()
+	},
 	map: {staticCubes: []},
 	copyToStorage: () => {
 		localStorage.setItem('map', JSON.stringify(mapCreator.map))
 	},
 	createMap: function(size) {
+		window.addEventListener('contextmenu', (event) => {
+			event.preventDefault()
+		})
+
 		var root = document.getElementById('root');
 		for(var j = 0;j < size[0];j++) {
 			var vertical = document.createElement('div');
@@ -22,35 +30,71 @@ var mapCreator = {
 				field.setAttribute("data-z", j)
 				field.setAttribute("data-x", i)
 				const _add = (e) => {
-					var X = 4.2*parseFloat(e.target.getAttribute('data-x'));
-					var Z = 4.2*parseFloat(e.target.getAttribute('data-z'));
+					if(e.target.getAttribute('data-status') == 'used') return;
+					console.log("🖱 mapCreator.isMousePressed = true; ", e)
+					e.target.setAttribute('data-status', 'used')
+					mapCreator.isMousePressed = true;
+					if(e.button == 2) {
+						console.log("1 🖱 right click detected!")
+					}
+					var X = 4.2 * parseFloat(e.target.getAttribute('data-x'));
+					var Z = 4.2 * parseFloat(e.target.getAttribute('data-z'));
 					this.map.staticCubes.push(
 						{
 							name: "wall_gen" + parseFloat(e.target.getAttribute('data-x')) + "_" + parseFloat(e.target.getAttribute('data-z')),
-							position: {x: X, y: 1, z: Z},
+							position: {x: X, y: byId('levelY').value, z: Z},
 							scale: [1, 1, 1],
 							texture: {
 								source: ["res/images/diffuse.png"],
 								mix_operation: "multiply"
 							}
 						})
-						this.copyToStorage()
-						e.target.style.background = 'gray';
+					this.copyToStorage()
+					e.target.style.background = 'gray';
 				}
-				field.addEventListener("click", _add)
+				field.addEventListener("mousedown", _add)
+				field.addEventListener("mouseup", () => {
+					mapCreator.isMousePressed = false;
+				})
+				// mousemove
+				const _add_OnHover = (e) => {
+					if(mapCreator.isMousePressed == false ||
+						e.target.getAttribute('data-status') == 'used'
+					) return;
+
+					e.target.setAttribute('data-status', 'used')
+					var X = 4.2 * parseFloat(e.target.getAttribute('data-x'));
+					var Z = 4.2 * parseFloat(e.target.getAttribute('data-z'));
+					this.map.staticCubes.push(
+						{
+							name: "wall_gen" + parseFloat(e.target.getAttribute('data-x')) + "_" + parseFloat(e.target.getAttribute('data-z')),
+							position: {x: X, y: byId('levelY').value, z: Z},
+							scale: [1, 1, 1],
+							texture: {
+								source: ["res/images/diffuse.png"],
+								mix_operation: "multiply"
+							}
+						})
+					this.copyToStorage()
+					e.target.style.background = 'gray';
+				}
+				field.addEventListener("mousemove", _add_OnHover)
+
 				vertical.appendChild(field)
 			}
 			root.appendChild(vertical);
 		}
 	},
-
 	run: () => {
 		mapCreator.createMap([byId('sizeX').value, byId('sizeY').value])
-		// byId("paramsPopup").()
+
+		byId('createMapDom').style.display = 'none';
+		byId('mapDom').style.display = 'block';
+	},
+	copyMap: () => {
+		navigator.clipboard.writeText(
+			localStorage.getItem('map')
+		);
 	}
 };
-
-// mapCreator.createMap([25, 25])
-
-
 window.mapCreator = mapCreator;
